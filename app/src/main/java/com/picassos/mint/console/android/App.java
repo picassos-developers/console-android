a1466685bfaa6792cfc663f7ca4298b7a7816842
Moe <moe@picassos.xyz>
2022-02-11 14:11:34 +0200

    # Projectt updated

--- app/src/main/java/com/picassos/mint/console/android/App.java
+++ app/src/main/java/com/picassos/mint/console/android/App.java
@@ -9,7 +9,14 @@ import android.util.Log;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.Constants;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.paypal.checkout.PayPalCheckout;
+import com.paypal.checkout.config.CheckoutConfig;
+import com.paypal.checkout.config.Environment;
+import com.paypal.checkout.config.SettingsConfig;
+import com.paypal.checkout.createorder.CurrencyCode;
+import com.paypal.checkout.createorder.UserAction;
 
 public class App extends Application {
 
@@ -25,9 +32,25 @@ public class App extends Application {
         super.onCreate();
         instance = this;
 
+        // init fresco
         Fresco.initialize(this);
 
-        // Init firebase.
+        // init paypal
+        CheckoutConfig config = new CheckoutConfig(
+                this,
+                Config.PAYPAL_CLIENT_ID,
+                Environment.SANDBOX,
+                String.format("%s://paypalpay", BuildConfig.APPLICATION_ID),
+                CurrencyCode.USD,
+                UserAction.PAY_NOW,
+                new SettingsConfig(
+                        true,
+                        false
+                )
+        );
+        PayPalCheckout.setConfig(config);
+
+        // init firebase.
         FirebaseApp.initializeApp(this);
 
         FirebaseMessaging.getInstance().getToken()
--- app/src/main/java/com/picassos/mint/console/android/Config.java
+++ app/src/main/java/com/picassos/mint/console/android/Config.java
@@ -1,10 +1,7 @@
 package com.picassos.mint.console.android;
 
 public class Config {
-    public final static String ENVATO_USERNAME = "PicassosTeam";
-    public final static String ENVATO_PERSONAL_TOKEN = "pckMxOhxIpU7BgpBfNVXpszgNw9MXan9";
-    public final static String ENVATO_COLLECTION_ID = "10876345";
-
+    public final static String PAYPAL_CLIENT_ID = "Ad8pf5HM3aSwixDLg989q8rz_-S41U37VKAL06SfIde-hlfKmQF8pgnrwP81jRckkDegd0ctGbUVcr2B";
     public final static String PRIVACY_POLICY_URL = "https://console.themintapp.com/policies/privacy";
     public final static String TERMS_OF_USE_URL = "https://console.themintapp.com/policies/terms";
 }
